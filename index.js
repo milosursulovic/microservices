70274fa3bbed813830d2585159d5fcb6f54fb619
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 22:25:02 +0200

    Implement auth service

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import UsersService from "./services/user.service.js";
 import EmailService from "./services/email.service.js";
+import AuthService from "./services/auth.service.js";
 
 async function startApp() {
   await UsersService.start();
   await EmailService.start();
+  await AuthService.start();
 
   try {
     const newUser = await UsersService.call("user.createUser", {
@@ -20,11 +22,18 @@ async function startApp() {
       content: "Thank you for sign up!",
     });
     console.log(emailResult);
+
+    const authResult = await AuthService.call("auth.authUser", {
+      username: newUser.username,
+      password: "password",
+    });
+    console.log("Auth result: ", authResult);
   } catch (error) {
     console.log("Error: ", error);
   } finally {
     await UsersService.stop();
     await EmailService.stop();
+    await AuthService.stop();
   }
 }
 
