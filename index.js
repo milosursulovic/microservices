36b99de62e59b1fc628cc7519cab4b0d503c7eaf
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 22:21:01 +0200

    Implement email service

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import UsersService from "./services/user.service.js";
+import EmailService from "./services/email.service.js";
 
 async function startApp() {
   await UsersService.start();
+  await EmailService.start();
 
   try {
     const newUser = await UsersService.call("user.createUser", {
@@ -11,10 +13,18 @@ async function startApp() {
     console.log("New user created: ", newUser);
     const users = await UsersService.call("user.getUsers");
     console.log("All users: ", users);
+
+    const emailResult = await EmailService.call("email.sendEmail", {
+      recipient: newUser.email,
+      subject: "Welcome to our platform!",
+      content: "Thank you for sign up!",
+    });
+    console.log(emailResult);
   } catch (error) {
     console.log("Error: ", error);
   } finally {
     await UsersService.stop();
+    await EmailService.stop();
   }
 }
 
