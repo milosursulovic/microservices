9a4c787e2b03ad59bf2b6a7de5d2bc0b6667ed56
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-05-28 12:54:57 +0200

    Init and add basic auth service code

---
+++ python/src/auth/server.py
@@ -0,0 +1,39 @@
+import jwt
+import datetime
+import os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# configuration
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_password"] = os.environ.get("MYSQL_password")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "Missing credentials", 401
+
+    # check db for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute("SELECT email, password FROM user WHERE email = %s",
+                      (auth.username))
+
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "Invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return "Invalid credentials", 401
